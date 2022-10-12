494d09c6d775e5a6631019d8be2ff3ebc3f1ecdf
redraiment <redraiment@gmail.com>
2022-10-12 10:07:46 +0800

    feat: finish local mode
    * Close #6 embedded pattern changeable.
    * Close #8 context data string.
    * Close #9 context data file.

--- src/me/zzp/jssp/core.clj
+++ src/me/zzp/jssp/core.clj
@@ -1,5 +1,15 @@
 (ns me.zzp.jssp.core
+  "JVM Scripting Server Pages"
+  (:require [me.zzp.jssp
+             [options :as options :refer [*global-options*]]
+             [template-engine :refer [render-file]]])
   (:gen-class))
 
 (defn -main
-  [& args])
+  [& args]
+  (let [{:keys [action payload]}
+        (options/validate args)]
+    (case action
+      :exit (.println System/err payload)
+      (binding [*global-options* (:options payload)]
+        (println (render-file (:template payload)))))))
--- src/me/zzp/jssp/options.clj
+++ src/me/zzp/jssp/options.clj
@@ -1,14 +1,105 @@
 (ns me.zzp.jssp.options
-  "Options")
+  "Global Options: command line arguments parser."
+  (:require [clojure.string :as cs]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]]
+            [cheshire.core :refer [parse-string]])
+  (:gen-class))
 
-(def statement-prefix
-  (System/getProperty "jssp.statement.prefix" "[!"))
+(def ^:dynamic *global-options*
+  "Global options:
+  * patterns: embedded patterns for expanding & executing.
+  * context: context data object"
+  nil)
 
-(def statement-suffix
-  (System/getProperty "jssp.statement.suffix" "!]"))
+(defn- pattern-parse
+  "Parse pattern pair."
+  [pattern]
+  (let [[prefix suffix] (cs/split pattern #"\s+")]
+    {:prefix prefix
+     :suffix suffix}))
 
-(def expression-prefix
-  (System/getProperty "jssp.expression.prefix" "[="))
+(defn- pattern-validate
+  "Validate pattern pair: prefix & suffix MUST not be blank."
+  [{:keys [prefix suffix]}]
+  (not (or (cs/blank? prefix)
+           (cs/blank? suffix))))
 
-(def expression-suffix
-  (System/getProperty "jssp.expression.suffix" "=]"))
+(defn- context-file-parse
+  "Parse JSON format context data file."
+  [file-name]
+  (when (and (or (cs/ends-with? file-name ".json")
+                 (cs/ends-with? file-name ".edn"))
+             (.exists (io/file file-name)))
+    (parse-string (slurp file-name))))
+
+(def ^:private command-line-options
+  [["-c" "--context-string JSON" "context data string"
+    :default {}
+    :parse-fn parse-string]
+   ["-f" "--context-file JSON-FILE" "context data file name"
+    :parse-fn context-file-parse
+    :validate [(comp not nil?) "invalid file content"]]
+   [nil "--expanding-statement PATTERN" "expanding statement pattern pair"
+    :default {:prefix "@!" :suffix "!@"}
+    :default-desc "@! !@"
+    :parse-fn pattern-parse
+    :validate [pattern-validate "both prefix and suffix must not be empty."]]
+   [nil "--expanding-expression PATTERN" "expanding expression pattern pair"
+    :default {:prefix "@=" :suffix "=@"}
+    :default-desc "@= =@"
+    :parse-fn pattern-parse
+    :validate [pattern-validate "both prefix and suffix must not be empty."]]
+   [nil "--executing-statement PATTERN" "executing statement pattern pair"
+    :default {:prefix "[!" :suffix "!]"}
+    :default-desc "[! !]"
+    :parse-fn pattern-parse
+    :validate [pattern-validate "both prefix and suffix must not be empty."]]
+   [nil "--executing-expression PATTERN" "executing expression pattern pair"
+    :default {:prefix "[=" :suffix "=]"}
+    :default-desc "[= =]"
+    :parse-fn pattern-parse
+    :validate [pattern-validate "both prefix and suffix must not be empty."]]
+   ["-h" "--help" "show help and exit"]])
+
+(defn usage
+  "Report program usage"
+  [summary]
+  (str "OVERVIEW
+
+  JVM Scripting Server page (also shortened as JSSP) is a polyglot templating system
+  that embeds JVM scripting language into a text document, similar to JSP, PHP, ASP,
+  and other server-side scripting languages.
+
+SYNOPSIS
+
+  jssp [options] FILE
+  jssp [-s | --server] [options] WORK-DIRECTORY
+
+OPTIONS
+
+"
+       summary
+       "
+
+Home page: https://github.com/redraiment/jssp
+E-mail bug reports to: redraiment@gmail.com"))
+
+(defn validate
+  "Validate command line arguments."
+  [args]
+  (let [{:keys [options arguments errors summary]}
+        (parse-opts args command-line-options)]
+    (cond
+      (:help options)
+      {:action :exit
+       :payload (usage summary)}
+
+      errors
+      {:action :exit
+       :payload (cs/join \newline errors)}
+
+      :else
+      {:action :render
+       :payload {:options options
+                 :template (first arguments)}})))

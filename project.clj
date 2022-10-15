6bedd10bca23c754551323c2550b84f9a4ecfd9d
redraiment <redraiment@gmail.com>
2022-10-15 14:55:26 +0800

    feat: upgrade to v0.2.1
    * Add tutorial in README.md.
    * Add nashorn engine for >= openjdk 14.
    * Close #19 build docker image.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.zzp/jssp "0.2.0"
+(defproject me.zzp/jssp "0.2.1"
   :description "JVM Scripting Server page (also shortened as JSSP) is a templating system that embeds JVM scripting language into a text document, similar to JSP, PHP, ASP, and other server-side scripting languages."
   :url "https://github.com/redraiment/jssp"
   :license {:name "Apache License - Version 2.0, January 2004"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.214"]
                  [cheshire "5.11.0"]
+                 [org.openjdk.nashorn/nashorn-core "15.4"]
                  [org.apache-extras.beanshell/bsh "2.0b6"]
                  [org.codehaus.groovy/groovy-jsr223 "3.0.13"]
                  [org.jruby/jruby "9.3.8.0"]]
